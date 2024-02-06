8eff933f1b5e098005ad78020cc771682bab0c26
Rutger <rutgervandeborne@gmail.com>
2024-02-06 10:40:30 +0100

    use case

--- src/main/java/be/intecbrussel/repository/ArticleRepository.java
+++ src/main/java/be/intecbrussel/repository/ArticleRepository.java
@@ -1,8 +1,17 @@
 package be.intecbrussel.repository;
 
+import be.intecbrussel.model.Article;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArticleRepository {
 
- public List<Article> listArticle;
+    public List<Article> listArticle = new ArrayList<>();
+    {
 
-
+    }
+    public List<Article> getArticle() {
+        return listArticle;
+    }
 }
--- src/main/java/be/intecbrussel/repository/ClientRepository.java
+++ src/main/java/be/intecbrussel/repository/ClientRepository.java
@@ -1,5 +1,16 @@
 package be.intecbrussel.repository;
 
+import be.intecbrussel.model.Article;
+import be.intecbrussel.model.Client;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClientRepository {
 
+    public List<Client> listClient = new ArrayList<>();
+
+    public List<Client> getClient() {
+        return listClient;
+    }
 }
